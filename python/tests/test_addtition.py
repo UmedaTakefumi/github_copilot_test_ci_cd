5bfada4254280edf23b1bb60cb8f363f67fdc765
UnknownUserName <example@example.jp>
2026-05-06 22:41:47 +0900

    test-commit

--- python/tests/test_addtition.py
+++ python/tests/test_addtition.py
@@ -20,24 +20,24 @@ class TestCalc(unittest.TestCase):
 
 if __name__ == "__main__":
 
-print("---------------------------")
-for temp in sys.path:
-  print(temp)
-print("---------------------------")
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-for temp in sys.path:
-  print(temp)
-print("---------------------------")
-
-print(os.getcwd())
-os.chdir('python')
-print(os.getcwd())
-print("---------------------------")
-
-#from Calc.calc import Calc
-#hoge = Calc()
-#result = hoge.simple_addition(2,3)
-#print(result)
-
-unittest.main()
-
+  print("---------------------------")
+  for temp in sys.path:
+    print(temp)
+  print("---------------------------")
+  sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+  for temp in sys.path:
+    print(temp)
+  print("---------------------------")
+  
+  print(os.getcwd())
+  os.chdir('python')
+  print(os.getcwd())
+  print("---------------------------")
+  
+  #from Calc.calc import Calc
+  #hoge = Calc()
+  #result = hoge.simple_addition(2,3)
+  #print(result)
+  
+  unittest.main()
+  
