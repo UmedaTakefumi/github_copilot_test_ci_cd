8702d54a64e8c250be5d0ed1118f6385831ea537
UnknownUserName <example@example.jp>
2026-05-06 22:40:31 +0900

    test-commit

--- python/tests/test_addtition.py
+++ python/tests/test_addtition.py
@@ -2,25 +2,6 @@ import sys
 import os
 import unittest
 
-#print("---------------------------")
-#for temp in sys.path:
-#  print(temp)
-#print("---------------------------")
-#sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-#for temp in sys.path:
-#  print(temp)
-#print("---------------------------")
-#
-#
-#print(os.getcwd())
-#os.chdir('python')
-#print(os.getcwd())
-#print("---------------------------")
-#
-#from Calc.calc import Calc
-#hoge = Calc()
-#result = hoge.simple_addition(2,3)
-#print(result)
 
 class TestCalc(unittest.TestCase):
   """ Calcクラスのテストケース """
@@ -38,5 +19,25 @@ class TestCalc(unittest.TestCase):
     self.assertEqual(result, 5)
 
 if __name__ == "__main__":
-  unittest.main()
+
+print("---------------------------")
+for temp in sys.path:
+  print(temp)
+print("---------------------------")
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+for temp in sys.path:
+  print(temp)
+print("---------------------------")
+
+print(os.getcwd())
+os.chdir('python')
+print(os.getcwd())
+print("---------------------------")
+
+#from Calc.calc import Calc
+#hoge = Calc()
+#result = hoge.simple_addition(2,3)
+#print(result)
+
+unittest.main()
 
