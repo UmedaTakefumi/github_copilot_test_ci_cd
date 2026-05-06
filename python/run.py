3e9ab3e33da1158d0c81025501436052c8ddeb48
UmedaTakefumi <umedatakefumi@gmail.com>
2026-05-07 00:23:08 +0900

    test-commit

--- python/run.py
+++ python/run.py
@@ -6,7 +6,15 @@ import sys
 
 from Calc.calc import Calc
 
-hoge = Calc()
-result = hoge.simple_addition(2,3)
-print(result)
+calc01 = Calc()
+result01 = calc01.simple_addition(1,2)
+print(result01)
+
+calc02 = Calc()
+result02 = calc02.simple_addition(2,3)
+print(result02)
+
+calc03 = Calc()
+result03 = calc03.simple_addition(3,4)
+print(result03)
 
