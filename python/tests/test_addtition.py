a1516756ee6ceda79722aeed4907e6381225e5b2
UmedaTakefumi <2947983+UmedaTakefumi@users.noreply.github.com>
2026-05-12 02:45:17 +0900

    test

--- python/tests/test_addtition.py
+++ python/tests/test_addtition.py
@@ -8,16 +8,22 @@ class TestCalc(unittest.TestCase):
     """ セットアップメソッド """
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+    ## see also: https://qiita.com/dokeita/items/521510ddd0e0e2317d7b
+    self.original_stdout = sys.stdout
+    sys.stdout = io.StrringIO()
+
     from Calc.calc import Calc
     self.calc    = Calc()
     self.counter = int(1);
 
   def tearDown(self):
     """ クリーンアップメソッド """
-    pass
+    output = sys.stdout.getvalue()
+    sys.stdout = self.original_stdout
+    print(output)
 
   def test_add_positive_num(self):
-    """ self.counter 加法のテスト """
+    """ 加法のテスト """
     result = self.calc.simple_addition(0,1)
     self.assertEqual(result, 1)
 
