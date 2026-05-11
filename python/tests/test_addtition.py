d56c9cf12d822744ba762ffbffcbb76878e0baed
UmedaTakefumi <2947983+UmedaTakefumi@users.noreply.github.com>
2026-05-12 03:12:27 +0900

    test

--- python/tests/test_addtition.py
+++ python/tests/test_addtition.py
@@ -5,6 +5,9 @@ import unittest
 
 class TestCalc(unittest.TestCase):
   """ Calcクラスのテストケース """
+  def hoge(self):
+    self.counter = ""
+
   def setUp(self):
     """ テストメソッド実行前に呼び出されるメソッド """
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -24,27 +27,27 @@ class TestCalc(unittest.TestCase):
     sys.stdout = self.original_stdout
     print(output)
 
-  def test_add_positive_num(self):
+  def test_add_positive_num_SerialNum_4c081197(self):
     """ 加法のテスト """
-    print(f"加法テスト: {self.uuidgen}")
+    # print(f"加法テスト: {self.uuidgen}")
     result = self.calc.simple_addition(0,1)
     self.assertEqual(result, 1)
 
-  def test_add_positive_num_0_1(self):
+  def test_add_positive_num_SerialNum_829f1d73(self):
     """ 0-1 加法のテスト """
-    print(f"加法テスト: {self.uuidgen}")
+    # print(f"加法テスト: {self.uuidgen}")
     result = self.calc.simple_addition(1,2)
     self.assertEqual(result, 3)
 
   def test_add_positive_num_0_2(self):
     """ 0-2 加法のテスト """
-    print(f"加法テスト: {self.uuidgen}")
+    # print(f"加法テスト: {self.uuidgen}")
     result = self.calc.simple_addition(2,3)
     self.assertEqual(result, 5)
 
   def test_add_positive_num_0_3(self):
     """ 0-3 加法のテスト """
-    print(f"加法テスト: {self.uuidgen}")
+    # print(f"加法テスト: {self.uuidgen}")
     result = self.calc.simple_addition(3,4)
     self.assertEqual(result, 7)
 
